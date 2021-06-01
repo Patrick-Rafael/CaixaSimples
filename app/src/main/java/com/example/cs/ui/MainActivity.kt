c3ff0eacc7a6d5e2257a301ca4b49ac3d0e500e8
Patrick Rafael <patrickrafael05@gmail.com>
2021-06-01 18:06:15 -0300

    Segunda Versão

--- app/src/main/java/com/example/cs/ui/MainActivity.kt
+++ app/src/main/java/com/example/cs/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.cs.R
 
 
@@ -12,14 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val botao: Button = findViewById(R.id.buttonLogin)
-
+        val botao: Button = findViewById(R.id.buttonEntrar)
+        val botaoCadastro: TextView = findViewById(R.id.textCadastro)
 
        botao.setOnClickListener {intent = Intent(applicationContext, tela_principal::class.java)
             startActivity(intent)}
 
-
-
+        botaoCadastro.setOnClickListener {intent = Intent(applicationContext, tela_cadastro::class.java)
+            startActivity(intent)}
 
     }
 }
---
+++ app/src/main/java/com/example/cs/ui/tela_cadastro.kt
@@ -0,0 +1,12 @@
+package com.example.cs.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cs.R
+
+class tela_cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_cadastro)
+    }
+}
